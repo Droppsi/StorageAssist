382811ffdafc2046bd90fba3dc39b0f0f1188629
Droppsi <85408168+Droppsi@users.noreply.github.com>
2022-07-10 13:55:34 +0200

    Added initial commit

---
+++ StorageAssist/StorageAssist/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StorageAssist.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StorageAssist.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
